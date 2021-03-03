25ac6fc564ffa7a732482a00e0fd503ba53fa0cd
barbara <barbarasymeonc@gmail.com>
2021-03-03 15:23:18 +0100

    samples and ressources

---
+++ samples/eliza_from_teacher/very-simple.py
@@ -0,0 +1,16 @@
+print("Let's introduce ourselves.")
+print("My name is Eliza. What is your name?")
+name = input()
+print("Hello " + name + ", nice to meet you.")
+
+print("How are you feeling today?")
+feeling = input()
+if feeling == "Good":
+  print("That's good.")
+elif feeling == "Awesome":
+  print("You are awesome!")
+elif feeling == "Sad":
+  print("Maybe I can help?")
+else:
+  print("I'm sorry you feel like that.")
+
