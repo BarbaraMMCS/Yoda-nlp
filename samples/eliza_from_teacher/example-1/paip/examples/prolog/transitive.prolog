cd14111e7022312dc434805e7f533ceef3caedb4
Barbara MMCS <51007181+BarbaraMMCS@users.noreply.github.com>
2021-03-16 20:47:44 +0100

    Delete samples directory

--- samples/eliza_from_teacher/example-1/paip/examples/prolog/transitive.prolog
+++
@@ -1,6 +0,0 @@
-# example of transitive relation
-
-<- likes(joe, judy)
-<- likes(judy, jorge)
-<- likes(?x, ?x)
-<- likes(?x, ?y) :- likes(?x, ?z), likes(?z, ?y)
--- samples/yoda-api/language/word.py
+++
@@ -1,5 +0,0 @@
-class Word(object):
-    def __init__(self, text, tag, category=None):
-        self.text = text
-        self.tag = tag
-        self.category = category
